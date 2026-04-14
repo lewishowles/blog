faaa0a41e5ec08255dfeafef26db4e7d830e8028
Lewis Howles <lewishowles@users.noreply.github.com>
2026-04-14 15:15:44 +0100

    Add Tailwind to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,19 @@ import path from "node:path";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
-	vite: {
+    site: "https://example.com",
+    integrations: [mdx(), sitemap()],
+    vite: {
 		resolve: {
 			alias: {
 				"@": path.resolve("./src")
 			}
-		}
-	}
+		},
+
+		plugins: [tailwindcss()]
+    }
 });
